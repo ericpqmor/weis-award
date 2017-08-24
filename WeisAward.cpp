8b3e1334bc617985fd827901fc667120e97488dc
ericpqmor <ericpqmor@gmail.com>
2017-08-24 02:16:31 -0300

    Changes to input/output directory

--- WeisAward.cpp
+++ WeisAward.cpp
@@ -2,13 +2,26 @@
 // Created by ericpqmor on 16/08/17.
 //
 
+#ifdef __linux__
+#include <unistd.h>
+#endif
+#ifdef _WIN32
+#include <direct.h>
+#endif
+
 #include <vector>
 #include "WeisCalculator.cpp"
 #include "WeisReader.cpp"
 
-
 int main()
 {
+    #ifdef __linux__
+        chdir(PROJECT_DIRECTORY);
+    #endif
+    #ifdef _WIN32
+        _chdir(PROJECT_DIRECTORY);
+    #endif
+
     WeisReader weisReader;
     WeisCalculator weisCalculator(weisReader.DetermineInput());
     weisCalculator.Run();
